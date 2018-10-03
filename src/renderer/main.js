8cc294b77641221f12c8d95866887a182947cea7
The Oddball <oddball@oddball.tf>
2018-10-03 01:29:15 -0700

    add Discord presence

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,12 +4,38 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import DiscordRPC from 'discord-rpc'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+const clientId = '477048998176489472'
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' })
+
+async function setActivity () {
+  if (!rpc) {
+    return
+  }
+
+  rpc.setActivity({
+    largeImageKey: 'steam_design_massive_png',
+    largeImageText: 'Steam.Design',
+    instance: false
+  })
+}
+
+rpc.on('ready', () => {
+  setActivity()
+
+  setInterval(() => {
+    setActivity()
+  }, 15e3)
+})
+
+rpc.login({ clientId }).catch(console.error)
+
 new Vue({
   components: { App },
   router,
